f2b88643192847879425de386444bd455fa9b1ae
pavellyko-cpu <pavel.lyko@kofola.cz>
2026-07-16 17:25:35 +0200

    Update data-loader.js

--- data-loader.js
+++ data-loader.js
@@ -1,29 +1,29 @@
 (function(){
     var API='https://script.google.com/macros/s/AKfycbzboESyGDt7_E4SHnL3SxddCeQbqKzfCSvmPWriLUyU8ozBIeyOEbob4gjQYBWUmMo0rg/exec';
     var F={CNG:'46,70',DIESEL:'35,10',KM:'574 091',T:'33 581',V:'77'};
+    function fmt(v){return v?String(v).replace('.',','):null;}
+    function fmtKM(v){return v?String(v).replace(/\B(?=(\d{3})+(?!\d))/g,' '):null;}
     function set(id,v){var e=document.getElementById(id);if(e&&v)e.textContent=v;}
     function apply(d){
-          set('km-month',d.KM||d.KM);
-          set('vehicle-count',d.VEHICLES||d.V);
-          set('vehicles-about',d.VEHICLES||d.V);
-          set('transit-count',d.TRANSITS||d.T);
-          set('diesel-price',(d.DIESEL||d.DIESEL)+' Kč/l s DPH');
-          set('cng-price',(d.CNG||d.CNG)+' Kč/kg s DPH');
-          set('diesel-price-srv',d.DIESEL);
-          set('cng-price-srv',d.CNG);
-          set('tb-diesel',(d.DIESEL)+' Kč/l s DPH');
-          set('tb-cng',(d.CNG)+' Kč/kg s DPH');
+          set('km-month',fmtKM(d.KM)||F.KM);
+          set('vehicle-count',d.VEHICLES||d.V||F.V);
+          set('vehicles-about',d.VEHICLES||d.V||F.V);
+          set('transit-count',fmtKM(d.TRANSITS||d.T)||F.T);
+          var diesel=fmt(d.DIESEL)||F.DIESEL;
+          var cng=fmt(d.CNG)||F.CNG;
+          set('diesel-price',diesel+' Kč/l s DPH');
+          set('cng-price',cng+' Kč/kg s DPH');
+          set('diesel-price-srv',diesel);
+          set('cng-price-srv',cng);
+          set('tb-diesel',diesel+' Kč/l s DPH');
+          set('tb-cng',cng+' Kč/kg s DPH');
     }
     function load(){
           var x=new XMLHttpRequest();
-          // cache-busting: unikátní parametr donutí prohlížeč i Google vždy
-          // stáhnout čerstvá data, jinak se občas vrací stará (zacachovaná) odpověď
-          var url=API+(API.indexOf('?')>-1?'&':'?')+'_ts='+Date.now();
+          var url=API+'?_ts='+Date.now();
           x.open('GET',url,true);
           x.timeout=5000;
-          try{
-            x.setRequestHeader('Cache-Control','no-cache, no-store, must-revalidate');
-          }catch(e){}
+          try{x.setRequestHeader('Cache-Control','no-cache, no-store, must-revalidate');}catch(e){}
           x.onload=function(){try{apply(JSON.parse(x.responseText));}catch(e){apply(F);}};
           x.onerror=x.ontimeout=function(){apply(F);};
           x.send();
