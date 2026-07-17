bd4d7c586efe964142a621e392ecaa1d8c3a869d
pavellyko-cpu <pavel.lyko@kofola.cz>
2026-07-17 07:40:57 +0200

    Add files via upload

--- translate.js
+++ translate.js
@@ -1,39 +1,11 @@
-/* Jazykový přepínač webu přes Google Translate (klientská integrace, zdarma) */
-
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: 'cs', autoDisplay: false },
-    'google_translate_element'
-  );
-}
-
-function gtranslateFireEvent(el, eventName) {
-  try {
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent(eventName, true, true);
-    el.dispatchEvent(evt);
-  } catch (e) {
-    el.fireEvent('on' + eventName);
-  }
-}
-
 function switchLang(lang) {
-  var attempts = 0;
-  var checkCondition = setInterval(function () {
-    var combo = document.querySelector('.goog-te-combo');
-    attempts++;
-    if (combo) {
-      combo.value = lang;
-      gtranslateFireEvent(combo, 'change');
-      clearInterval(checkCondition);
-    } else if (attempts > 40) {
-      clearInterval(checkCondition);
-    }
-  }, 200);
+  if (lang === 'cs') {
+    // Zpět na originál
+    window.location.href = window.location.href
+      .replace(/^https:\/\/translate\.google\.com.*?u=/, '')
+      .replace(/^.*santatrans/, 'https://santatrans');
+    return;
+  }
+  var url = 'https://translate.google.com/translate?sl=cs&tl=' + lang + '&u=' + encodeURIComponent(window.location.href);
+  window.location.href = url;
 }
-
-(function () {
-  var s = document.createElement('script');
-  s.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-  document.head.appendChild(s);
-})();
