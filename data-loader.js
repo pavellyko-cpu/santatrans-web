1f6222c2c5d0207093389c74027917b9b8a23d77
pavellyko-cpu <pavel.lyko@kofola.cz>
2026-07-22 09:02:42 +0200

    data-loader: read from Apps Script directly

--- data-loader.js
+++ data-loader.js
@@ -1,26 +1,30 @@
 (function(){
-  var DB='https://santatrans-web-default-rtdb.europe-west1.firebasedatabase.app/ceny.json';
-  var F={CNG:'46,70',DIESEL:'35,10',KM:'574 091',T:'33 581',V:'77'};
-  function fmt(v){return v?String(v).replace('.',','):null;}
-  function fmtKM(v){return v?String(v).replace(/\B(?=(\d{3})+(?!\d))/g,' '):null;}
-  function set(id,v){var e=document.getElementById(id);if(e&&v)e.textContent=v;}
-  function apply(d){
-    var diesel=fmt(d.DIESEL)||F.DIESEL;
-    var cng=fmt(d.CNG)||F.CNG;
-    set('diesel-price',diesel+' Kč/l s DPH');
-    set('cng-price',cng+' Kč/kg s DPH');
-    set('diesel-price-srv',diesel);
-    set('cng-price-srv',cng);
-    set('tb-diesel',diesel+' Kč/l s DPH');
-    set('tb-cng',cng+' Kč/kg s DPH');
-  }
-  function load(){
-    var x=new XMLHttpRequest();
-    x.open('GET',DB+'?_ts='+Date.now(),true);
-    x.timeout=5000;
-    x.onload=function(){try{apply(JSON.parse(x.responseText)||F);}catch(e){apply(F);}};
-    x.onerror=x.ontimeout=function(){apply(F);};
-    x.send();
-  }
-  load();
+    var API='https://script.google.com/macros/s/AKfycbzboESyGDt7_E4SHnL3SxddCeQbqKzfCSvmPWriLUyU8ozBIeyOEbob4gjQYBWUmMo0rg/exec';
+    var F={CNG:'46,70',DIESEL:'35,10',KM:'574 091',T:'33 581',V:'77'};
+    function fmt(v){return v?String(v).replace('.',','):null;}
+    function fmtKM(v){return v?String(v).replace(/\B(?=(\d{3})+(?!\d))/g,' '):null;}
+    function set(id,v){var e=document.getElementById(id);if(e&&v)e.textContent=v;}
+    function apply(d){
+          var diesel=fmt(d.DIESEL)||F.DIESEL;
+          var cng=fmt(d.CNG)||F.CNG;
+          set('tb-diesel',diesel+' K\u010d/l s DPH');
+          set('tb-cng',cng+' K\u010d/kg s DPH');
+          set('diesel-price',diesel+' K\u010d/l s DPH');
+          set('cng-price',cng+' K\u010d/kg s DPH');
+          set('diesel-price-srv',diesel);
+          set('cng-price-srv',cng);
+          set('km-month',fmtKM(d.KM)||F.KM);
+          set('vehicle-count',d.VEHICLES||F.V);
+          set('vehicles-about',d.VEHICLES||F.V);
+          set('transit-count',fmtKM(d.TRANSITS)||F.T);
+    }
+    function load(){
+          var x=new XMLHttpRequest();
+          x.open('GET',API+'?_ts='+Date.now(),true);
+          x.timeout=8000;
+          x.onload=function(){try{apply(JSON.parse(x.responseText));}catch(e){apply(F);}};
+          x.onerror=x.ontimeout=function(){apply(F);};
+          x.send();
+    }
+    load();
 })();
